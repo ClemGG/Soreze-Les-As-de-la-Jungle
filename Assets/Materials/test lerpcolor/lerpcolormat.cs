e70d1aa2a9fe37a64eceafaf2725161697bc8b9c
Clement Guichard <c.guichard508@laposte.net>
2020-07-21 12:19:41 +0200

    push assets no 3

---
+++ Assets/Materials/test lerpcolor/lerpcolormat.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lerpcolormat : MonoBehaviour
+{
+    Material mat;
+    public AnimationCurve lerpCurve;
+    private float t;
+    float sign = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = GetComponent<MeshRenderer>().sharedMaterial;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(t > 1f || t < 0f)
+        {
+            sign *= -1f;
+        }
+
+        t += Time.deltaTime * sign;
+
+
+        mat.SetColor("_Color", Color.Lerp(Color.cyan, Color.magenta, lerpCurve.Evaluate(t)));
+    }
+}
